a957723ae08b4d7ec7fe5d0dcc3927c5d436a07e
luiz.martinelli <luiz@microsys.inf.br>
2022-07-05 13:22:37 -0300

    Finalização

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps/maps.dart';
+
+void main() {
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MapsPage(),
+    );
+  }
+}
+
+
+
+
---
+++ lib/maps.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+
+class MapsPage extends StatefulWidget {
+  const MapsPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapsPage> createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 150,
+          child: Column(
+            children: [
+              Flexible(
+                child: Stack(
+                   alignment: AlignmentDirectional.center,
+                  children: [
+                    FlutterMap(
+                    options: MapOptions(
+                      
+                      center: LatLng(-26.7731843, -53.2030883),
+                    ),
+                    layers: [
+                      TileLayerOptions(
+                        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        subdomains: ['a', 'b', 'c'],
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      top: 2, 
+                      right: 280,
+                      child: ElevatedButton.icon(onPressed: (){}, icon: const Icon(Icons.near_me), label: const Text("Itinerários"))
+                    ),
+                  ]
+                )
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
